71e1040ea14e8c6cefb732749aca6635d60d326a
Edvin Salo <best.fast@Edvins-Mac.local>
2016-03-05 10:17:55 -0800

    number_of_tags applied to question edit

--- questions/question_edit.js
+++ questions/question_edit.js
@@ -4,9 +4,22 @@ if (Meteor.isClient) {
     $(".collapsible").collapsible();
   });
 
+  Template.question_edit.onCreated(function() {
+    this.number_of_tags = new ReactiveVar(this.data.tags === undefined ? 0 : this.data.tags.length);
+  });
+
   Template.question_edit.helpers({
     'number_of_tags': function() {
-      return $("input[name^='tags']").length;
+      return Template.instance().number_of_tags.get();
+    }
+  });
+
+  Template.question_edit.events({
+    'blur input[type="text"][name^="tags"]': function(event, template) {
+      template.number_of_tags.set($('input[type="text"][name^="tags"]').length);
+    },
+    'click button.autoform-remove-item[data-autoform-field="tags"]': function(event, template) {
+      template.number_of_tags.set($('input[type="text"][name^="tags"]').length - 1);
     }
   });
 
