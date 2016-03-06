1feaea8501af425d47b6a5068af4a9ce7e9a9a31
Sid Kwakkel <kwakkels@ae.ca>
2016-03-06 12:47:17 -0800

    Trying to fix things now

--- books/book_add.js
+++ books/book_add.js
@@ -3,7 +3,6 @@ if (Meteor.isClient) {
     $.publish('page_changed',"books");
   });
 
-<<<<<<< HEAD
   Template.book_add.helpers({
 
   });
@@ -12,13 +11,10 @@ if (Meteor.isClient) {
 
   });
 
-=======
->>>>>>> 165c2f605c6d231195bdc3d3f0392635ecc39218
   AutoForm.hooks({
     'insert_book': {
       before: {
         insert: function(doc) {
-<<<<<<< HEAD
           doc.pages = [];
           return doc;
         }
@@ -30,11 +26,6 @@ if (Meteor.isClient) {
         $(".back-button").click();
         return false;
       },
-=======
-          return doc;
-        }
-      },
->>>>>>> 165c2f605c6d231195bdc3d3f0392635ecc39218
       onSuccess: function(formType, result) {
         $.publish('toast',["Your addition was successful!","Book Saved",'success']);
       },
