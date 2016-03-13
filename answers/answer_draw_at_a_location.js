8a18e4cec4ccb9fe229c83b08c22846770996997
Sid Kwakkel <kwakkels@ae.ca>
2016-03-13 09:45:18 -0700

    Added zoom to answer map. formatting changes.

--- answers/answer_draw_at_a_location.js
+++ answers/answer_draw_at_a_location.js
@@ -1,41 +1,44 @@
 if (Meteor.isClient) {
+  var map;
+
   Template.answer_draw_at_a_location.onRendered(function() {
     $.publish('page_changed',"buildings");
 
     // building = Buildings.findOne({_id: this.building_id}).fetch();
-    if(this.data.building)
-    {
+    if(this.data.building) {
       // building = Buildings.findOne({_id: this.building._id}).fetch();
       let building = this.data.building;
       let imageUrl, imageBounds;
 
-      map = L.map('map', {zoomControl: false}).setView([building.location.coordinates[1], building.location.coordinates[0]], 5);
+      map = L.map('answer-map', {zoomControl: false}).setView([building.location.coordinates[1], building.location.coordinates[0]], 15);
 
-      L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png').addTo(map);
+      //only display open streetmap for web users
+      if(!Meteor.isCordova){
+        L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png').addTo(map);
+      }
+
       if (building.picture) {
         imageUrl = Images.findOne({_id: building.picture}).url();
+        if(building.bounding_box){
+          imageBounds = JSON.parse(building.bounding_box);
+          L.imageOverlay(imageUrl, imageBounds).addTo(map);
+        }
       }
-      else {
-        imageUrl = 'http://localhost:3000/images/Hospital.jpg';
-      }
-
-      if(building.bounding_box)
-      {
-        imageBounds = JSON.parse(building.bounding_box);
-      }
-      else 
-      {
-        imageBounds = [[62.4658007641611,-114.450533917207],[82.4670926867988,-84.44751361509]];  
-      }      
-      
-      L.imageOverlay(imageUrl, imageBounds).addTo(map);
     }
-    
   });
 
   Template.answer_draw_at_a_location.helpers({
     'questions_in_group': function() {
       return Question_In_Group.find({group_id: this.group._id},{sort: {sort_order: 1}}).fetch();
+    }
+  });
+
+  Template.answer_draw_at_a_location.events({
+    'click #btn_zoomin': function() {
+      map.zoomIn();
+    },
+    'click #btn_zoomout': function() {
+      map.zoomOut();
     }
   });
 }
