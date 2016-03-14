12d70ae3d4ae7b64015bdba0a190f60367359493
Sid Kwakkel <kwakkels@ae.ca>
2016-03-13 18:08:40 -0700

    Added Decision Points to Question in groups UI. Need to actually make it save

--- question_in_group/question_in_group_view.js
+++ question_in_group/question_in_group_view.js
@@ -2,6 +2,8 @@ if (Meteor.isClient) {
   Template.question_in_group_view.onRendered(function() {
     $.publish('page_changed',"books");
     $('ul.tabs').tabs();
+    $.publish("toast",["Select","Select",'info']);
+    $('select').material_select();
   });
 
   Template.question_in_group_view.helpers({
@@ -19,6 +21,44 @@ if (Meteor.isClient) {
       var group = Question_Groups.findOne({_id: this.group_id});
       var page = Pages.findOne({_id: group.page_id});
       return Books.findOne({_id: page.book_id});
+    },
+    'is_geo': function() {
+      var question = Questions.findOne({_id: this.question_id});
+      if(question.type === "Geo-Point" || question.type === "Geo-Area") {
+        return true;
+      }
+      return false;
+    },
+    'is_numeric': function() {
+      var question = Questions.findOne({_id: this.question_id});
+      if(question.type === "Numeric" || question.type === "Year") {
+        return true;
+      }
+      return false;
+    },
+    'is_mc': function() {
+      var question = Questions.findOne({_id: this.question_id});
+      if(question.type === "Multiple Choice") {
+        return true;
+      }
+      return false;
     }
   });
+
+  Template.question_in_group_view.events({
+    "click #btn_add_mc": function(event, template){
+      event.preventDefault();
+      $.publish('toast',["This should add value: "+$("#value").val()+" and label: "+$("#label").val(),"Add Clicked",'info']);
+      $("#value").val("");
+      $("#label").val("");
+    },
+    "click #btn_add_num": function(event, template){
+      event.preventDefault();
+      $.publish('toast',["This should add min: "+$("#min").val()+", max: "+$("#max").val()+" and label: "+$("#label").val(),"Add Clicked",'info']);
+      $("#min").val("");
+      $("#max").val("");
+      $("#label").val("");
+    }
+  });
+
 }
