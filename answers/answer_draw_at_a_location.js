3169130c6e59a214790744aae9df0c0716a95431
Edvin Salo <best.fast@Edvins-Mac.local>
2016-03-15 19:07:14 -0700

    answer add, delete, remove notification added by $.publsh

--- answers/answer_draw_at_a_location.js
+++ answers/answer_draw_at_a_location.js
@@ -10,7 +10,10 @@ if (Meteor.isClient) {
     if (answer)
     {
       // Answers.remove(answer._id);
-       Meteor.call("deleteAnswer", answer._id);
+       Meteor.call("deleteAnswer", answer._id, function(error, result) {
+        if(error) $.publish('toast',[error.reason,"An error occurred",'error']);
+        else $.publish('toast',["Your remove answer was successful!","Answer Deleted",'success']);
+       });
     }
   };
 
@@ -59,12 +62,18 @@ if (Meteor.isClient) {
       // Answers.update(originalAnswer._id, {
       //   $set: answerObject
       // });
-      Meteor.call("updateAnswer", originalAnswer._id, answerObject);
+      Meteor.call("updateAnswer", originalAnswer._id, answerObject, function(error, result) {
+        if(error) $.publish('toast',[error.reason,"An error occurred",'error']);
+        else $.publish('toast',["Your answer was updated successful!","Answer Modified",'success']);
+      });
     }
     else
     {
       // Answers.insert(answerObject);
-      Meteor.call("insertAnswer", answerObject);
+      Meteor.call("insertAnswer", answerObject, function(error, result) {
+        if(error) $.publish('toast',[error.reason,"An error occurred",'error']);
+        else $.publish('toast',["New answer was created successful!","Answer Created",'success']);
+      });
     }
 
   };
@@ -186,78 +195,6 @@ if (Meteor.isClient) {
 
   Template.answer_draw_at_a_location.onRendered(function() {
 
-    // this.autorun(function() {
-    //   let currData = Template.currentData();
-
-    //   $.publish('page_changed',"buildings");
-
-    //   if (currData.building && currData.group.use_map) {
-    //     Meteor.defer(function() {
-
-    //       // building = Buildings.findOne({_id: this.building._id}).fetch();
-    //       let building = currData.building;
-    //       let imageUrl, imageBounds;
-
-    //       map = L.map('answer-map', {zoomControl: false, minZoom: 14}).setView([building.location.coordinates[1], building.location.coordinates[0]], 15);
-
-    //       //only display open streetmap for web users
-    //       if(!Meteor.isCordova){
-    //        L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png').addTo(map);
-    //       }
-
-    //       if (building.picture) {
-    //         imageUrl = Images.findOne({_id: building.picture}).url();
-    //         if(building.bounding_box){
-    //           imageBounds = JSON.parse(building.bounding_box);
-    //           L.imageOverlay(imageUrl, imageBounds).addTo(map);
-    //           //$.publish('toast',['Drawing an ImageOverlay','Image Overlay','info']);
-    //         }
-    //       } else {
-    //         $.publish('toast',['Functionality may be restricted','No Aerial Image!','warning']);
-    //       }
-
-    //       L.Icon.Default.imagePath = Meteor.absoluteUrl() + 'packages/bevanhunt_leaflet/images';
-
-    //       drawnItems = L.featureGroup().addTo(map);
-
-    //       map.addControl(new L.Control.Draw({
-    //         draw: {
-    //           polyline: false,
-    //           circle: false,
-    //           rectangle: false
-    //         },
-    //         edit: {
-    //           featureGroup: drawnItems,
-    //           edit: false,
-    //           remove: true
-    //         }
-    //       }));
-
-    //       map.on('draw:created', function(event) {
-    //         var layer = event.layer;
-    //         // console.log(event.layer);
-    //         // console.log(event.layerType);
-    //         // console.log(drawnItems);
-    //         var feature = {
-    //           options: event.layer.options,
-    //           layerType: event.layerType,
-    //           _id: marker_id
-    //         };
-    //         switch (event.layerType) {
-    //         case 'marker':
-    //           feature.latlng = event.layer._latlng;
-    //           break;
-    //         case 'polygon':
-    //           feature.latlngs = event.layer._latlngs;
-    //           break;
-    //         }
-    //         // console.log(feature);
-    //         markersInsert(feature);
-    //         handleButtons(feature._id);
-    //       });
-    //     });
-    //   }
-    // });
   });
 
   Template.registerHelper("has_decision_point", function(qig_id){
@@ -484,15 +421,15 @@ Meteor.methods({
       throw new Meteor.Error("not-authorized");
     }
 
-    Answers.insert(answerObject);
+    return Answers.insert(answerObject);
     // $.publish('toast',['New Answer inserted','Answer Added!','info']);
   },
   deleteAnswer: function (id) {
-    Answers.remove(id);
+    return Answers.remove(id);
     // $.publish('toast',['Corresponding Answer removed','Answer Deleted!','info']);
   },
   updateAnswer: function (id, answerObject) {
-    Answers.update(id, { $set: answerObject });
+    return Answers.update(id, { $set: answerObject });
     // $.publish('toast',['Corresponding Answer modified','Answer Updated!','info']);
   }
 });
