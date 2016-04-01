23582d4aab4853ed75bd4e80c8edfe6ad826a755
Edvin Salo <best.fast@Edvins-Mac.local>
2016-04-01 05:05:11 -0700

    user info save corrected in user_edit

--- users/user_edit.js
+++ users/user_edit.js
@@ -3,13 +3,37 @@ if (Meteor.isClient) {
     $.publish('page_changed',"users");
   });
 
+  Template.user_edit.helpers({
+    checkedIf: function(role) {
+      // return roles.default_group.[0] == role;
+      return Template.instance().data.roles.default_group[0] == role ? 'checked' : '';
+    }
+  });
+
   Template.user_edit.events({
     'click #submit_user_edit_info': function(event) {
       event.preventDefault();
-      var id = $('[name=create_user_id]').val();
-      var email = $('[name=create_user_email]').val();
-      var fullname = $('[name=edit_user_fullname]').val();
-      Meteor.users.update( {_id: id }, { $set: {'profile.fullname': fullname, 'emails[0].address': email} });
+      if( !$('[name=edit_user_email]').hasClass("valid") || !$('[name=edit_user_fullname]').hasClass("valid") ) 
+      {
+        $.publish('toast', ["You have to fill Full Name and eMail Address correctly!", "User Info Save Failed", 'warning']);
+        return false;
+      }
+      var id = Template.instance().data._id;
+      var email = $('[name=edit_user_email]').val();
+      var fullname = $('[name=edit_user_fullname]').val();      
+      
+      Meteor.call("userUpdate", id, {'profile.fullname': fullname, 'emails[0].address': email}, function(error, result) {
+        if(error) $.publish('toast',[error.reason,"An error occurred",'error']);
+        else $.publish('toast',["Your change to user info accepted","User Info Saved",'success']);
+      });
+
+      // Meteor.users.update( {_id: id }, { $set: {'profile.fullname': fullname, 'emails[0].address': email} });  
     }
   });
 }
+
+Meteor.methods({
+  userUpdate: function (id, userInfo) {
+    return Meteor.users.update( {_id: id }, { $set: userInfo });
+  }
+});
